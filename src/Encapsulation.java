be25920c7a88897ea94863fa8923252b0b01a793
Suna Kara <karacalisuna912@gmail.com>
2022-04-09 22:41:48 +0300

    Encapsulation created

--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -1,3 +1,19 @@
 public class Encapsulation {
     private String musicInstrument;
+    public  Encapsulation(){}
+    public  String getMusicInstrument(){
+        return  musicInstrument;
+    }
+
+    public  void  setMusicInstrument(String musicInstrument){
+        this.musicInstrument=musicInstrument;
+    }
+
+
+
+
+
+
+
+
 }
